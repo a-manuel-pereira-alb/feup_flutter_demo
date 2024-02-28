05e24d2100dc0f9f98badd66333af4a3e611d556
a-manuel-pereira-alb <a-manuel-pereira@alticelabs.com>
2024-02-28 11:43:37 +0000

    - Changed the App Structure; - Changed the name of some Widgets.

---
+++ lib/app.dart
@@ -0,0 +1,18 @@
+import 'package:feup_flutter_demo/ui/counter_page.dart';
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'FEUP Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const CounterPage(title: 'FEUP Flutter Demo'),
+    );
+  }
+}
---
+++ lib/ui/counter_page.dart
@@ -0,0 +1,62 @@
+import 'package:feup_flutter_demo/components/my_colored_box.dart';
+import 'package:feup_flutter_demo/components/my_stateful_widget.dart';
+import 'package:feup_flutter_demo/components/my_widget.dart';
+import 'package:flutter/material.dart';
+
+class CounterPage extends StatefulWidget {
+  const CounterPage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<CounterPage> createState() => _CounterPageState();
+}
+
+class _CounterPageState extends State<CounterPage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Row(
+              children: [
+                MyColoredBox(),
+                MyColoredBox(),
+                MyColoredBox(),
+                MyColoredBox(),
+              ],
+            ),
+            const MyWidget(name: 'Hi FEUP!'),
+            const MyStatefulWidget(),
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
