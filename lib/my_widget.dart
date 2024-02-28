9936bdfe7a4400f200563cebd5e8c33626b097a4
a-manuel-pereira-alb <a-manuel-pereira@alticelabs.com>
2024-02-28 10:36:20 +0000

    - Added a Widget.

---
+++ lib/my_widget.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/widgets.dart';
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox();
+  }
+}
