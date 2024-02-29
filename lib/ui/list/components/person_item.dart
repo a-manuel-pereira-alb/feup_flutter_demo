f87287d5ef9dcb41e29152ba57261673872c1476
a-manuel-pereira-alb <a-manuel-pereira@alticelabs.com>
2024-02-29 16:33:31 +0000

    - Added a new dependency; - Added a new feature to PersonItem; - Added new logic to ListPage.

--- lib/ui/list/components/person_item.dart
+++ lib/ui/list/components/person_item.dart
@@ -1,55 +1,72 @@
 import 'package:feup_flutter_demo/models/person_model.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class PersonItem extends StatelessWidget {
   final PersonModel person;
   final VoidCallback onTap;
+  final VoidCallback onDeleteTap;
 
   const PersonItem({
     Key? key,
     required this.person,
     required this.onTap,
+    required this.onDeleteTap,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () => onTap.call(),
-      child: Container(
-        height: 80,
-        padding: const EdgeInsets.all(10),
-        decoration: BoxDecoration(
-          color: Colors.blueAccent.shade100,
-          borderRadius: const BorderRadius.all(
-            Radius.circular(12),
+    return Slidable(
+      key: ValueKey(person.name),
+      endActionPane: ActionPane(
+        motion: const ScrollMotion(),
+        children: [
+          SlidableAction(
+            onPressed: (context) => onDeleteTap.call(),
+            foregroundColor: Colors.red,
+            icon: Icons.delete,
+            label: 'Delete',
           ),
-        ),
-        child: Row(
-          children: [
-            Container(
-              height: 40,
-              width: 40,
-              decoration: const BoxDecoration(
-                color: Colors.blue,
-                shape: BoxShape.circle,
-              ),
-              child: const Icon(
-                Icons.person,
-                color: Colors.white,
-                //size: 40,
-              ),
+        ],
+      ),
+      child: GestureDetector(
+        onTap: () => onTap.call(),
+        child: Container(
+          height: 80,
+          padding: const EdgeInsets.all(10),
+          decoration: BoxDecoration(
+            color: Colors.blueAccent.shade100,
+            borderRadius: const BorderRadius.all(
+              Radius.circular(12),
             ),
-            const SizedBox(width: 10),
-            Expanded(
-              child: Text(
-                person.name,
-                style: const TextStyle(
-                  color: Colors.black87,
-                  fontSize: 18,
+          ),
+          child: Row(
+            children: [
+              Container(
+                height: 40,
+                width: 40,
+                decoration: const BoxDecoration(
+                  color: Colors.blue,
+                  shape: BoxShape.circle,
+                ),
+                child: const Icon(
+                  Icons.person,
+                  color: Colors.white,
+                  //size: 40,
                 ),
               ),
-            ),
-          ],
+              const SizedBox(width: 10),
+              Expanded(
+                child: Text(
+                  person.name,
+                  style: const TextStyle(
+                    color: Colors.black87,
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/ui/list/list_page.dart
+++ lib/ui/list/list_page.dart
@@ -1,7 +1,10 @@
+import 'dart:math';
+
 import 'package:feup_flutter_demo/models/person_model.dart';
 import 'package:feup_flutter_demo/ui/list/components/person_item.dart';
 import 'package:feup_flutter_demo/ui/list/details_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class ListPage extends StatefulWidget {
   const ListPage({
@@ -13,9 +16,70 @@ class ListPage extends StatefulWidget {
 }
 
 class _ListPageState extends State<ListPage> {
+  late List<PersonModel> _people;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _people = _getPeople();
+  }
+
   @override
   Widget build(BuildContext context) {
-    final List<PersonModel> people = [
+    return Material(
+      child: Stack(
+        children: [
+          // List
+          Positioned.fill(
+            child: RefreshIndicator.adaptive(
+              onRefresh: () async => _restorePeople(),
+              child: ListView.builder(
+                padding: const EdgeInsets.all(20).copyWith(bottom: 80),
+                itemCount: _people.length,
+                itemBuilder: (context, index) {
+                  final isLastPerson = _people.last == _people[index];
+
+                  return Padding(
+                    padding: EdgeInsets.only(bottom: isLastPerson ? 0 : 20),
+                    child: PersonItem(
+                      person: _people[index],
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => DetailsPage(
+                              person: _people[index],
+                            ),
+                          ),
+                        );
+                      },
+                      onDeleteTap: () => _deletePerson(person: _people[index]),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ),
+
+          // Add person
+          Positioned(
+            right: 20,
+            bottom: 20,
+            child: IconButton(
+              icon: const Icon(Icons.add_circle),
+              iconSize: 40,
+              onPressed: () => _addPerson(),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  /// Get people.
+  List<PersonModel> _getPeople() {
+    return [
       const PersonModel(name: 'Maria'),
       const PersonModel(name: 'João'),
       const PersonModel(name: 'Zé'),
@@ -36,32 +100,41 @@ class _ListPageState extends State<ListPage> {
       const PersonModel(name: 'Vitor'),
       const PersonModel(name: 'Francisca'),
     ];
+  }
 
-    return Material(
-      child: ListView.builder(
-        padding: const EdgeInsets.all(20),
-        itemCount: people.length,
-        itemBuilder: (context, index) {
-          final isLastPerson = people.last == people[index];
-
-          return Padding(
-            padding: EdgeInsets.only(bottom: isLastPerson ? 0 : 20),
-            child: PersonItem(
-              person: people[index],
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => DetailsPage(
-                      person: people[index],
-                    ),
-                  ),
-                );
-              },
-            ),
-          );
-        },
-      ),
+  /// Add a person.
+  void _addPerson() {
+    // Create a person with a random name
+    final newPerson = PersonModel(
+      name: String.fromCharCodes(List.generate(12, (index) => Random().nextInt(33) + 89)),
     );
+
+    List<PersonModel> updatedPeople = List.from(_people);
+
+    updatedPeople.add(newPerson);
+
+    setState(() {
+      _people = updatedPeople;
+    });
+  }
+
+  /// Delete [person].
+  void _deletePerson({
+    required PersonModel person,
+  }) {
+    List<PersonModel> updatedPeople = List.from(_people);
+
+    updatedPeople.remove(person);
+
+    setState(() {
+      _people = updatedPeople;
+    });
+  }
+
+  /// Restore people.
+  void _restorePeople() {
+    setState(() {
+      _people = _getPeople();
+    });
   }
 }
